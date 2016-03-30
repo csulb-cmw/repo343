26e6e65a022c6d6c5adde92b5b52b1e67bd42ebf
Cory Knapp <knapp.cory@gmail.com>
2016-03-29 22:13:20 -0700

    changed tests.py to use built in python unit testing

--- tests.py
+++ tests.py
@@ -1,18 +1,16 @@
+import unittest
 from commit import ignore 
 
-def main():
-    test_cases = [
-            ['user/documents/test', False ],
-            ['user/documents/.test', True ],
-            ['user/documents/repo343/test.py', False],
-            ['user/repo343/test/another/test.py', False],
-            ['user/test/program.o', False],
-            ]
-    for test in test_cases:
-        print 'testing "' + test[0] +'"...'
-        if ignore(test[0]) == test[1]:
-            print 'passed!'
-        else:
-            print 'FAILED!!!!'
+class CommitTests(unittest.TestCase):
+    """Tests for `commit.py`."""
 
-main()
+    def test_ignore(self):
+        """Is five successfully determined to be prime?"""
+        self.assertFalse(ignore('user/documents/test'))
+        self.assertTrue(ignore('user/documents/.test'))
+        self.assertTrue(ignore('user/documents/repo343/test.py'))
+        self.assertTrue(ignore('user/repo343/test/another/test.py'))
+        self.assertTrue(ignore('user/test/program.o'))
+
+if __name__ == '__main__':
+    unittest.main()
